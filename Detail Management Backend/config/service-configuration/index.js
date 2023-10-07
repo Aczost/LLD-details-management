1da5fd66c5fdea3d736fc0e7f4ffe6d19c8d2855
Amit-Tejwani <amit.tejwani@rapidops.com>
2023-10-07 11:50:46 +0530

    changed index file in config

--- Detail Management Backend/config/service-configuration/index.js
+++ Detail Management Backend/config/service-configuration/index.js
@@ -1,3 +1,4 @@
 const env = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
-const config = require(`./${env}.js`);
+// const config = require(`./${env}.js`);
+const config = require(`./development`);
 module.exports = config;
