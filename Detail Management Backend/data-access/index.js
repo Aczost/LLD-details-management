0345968c37342017706aaa6ef7ab63a64dc0a0c9
MugishBeldar <mugishbeldar333@gmail.com>
2023-11-06 13:19:32 +0530

    update db connection

--- Detail Management Backend/data-access/index.js
+++ Detail Management Backend/data-access/index.js
@@ -2,6 +2,7 @@ const mysql2 = require("mysql2")
 const config = require("../config")
 const mongoose = require('mongoose');
 const DetailsModel = require('../migration-mongodb/detailsSchema');
+
 const mysql = mysql2.createPool({
     host: config.mysql.host,
     user: config.mysql.username,
@@ -9,23 +10,22 @@ const mysql = mysql2.createPool({
     database: config.mysql.database
 }).promise();
 
+mongoose.connect(config.mongoDb.host, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
-// mongoose.connect(config.mongoDb.host, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// });
-
-// const db = mongoose.connection;
-
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-// db.once('open', function () {
-//     console.log('Connected to MongoDB');
-// });
-
-mongoose.connect(config.mongoDb.host);
 const db = mongoose.connection;
-db.on('error', err => console.log("Error",err))
-db.once('open',()=>console.info('Connected to the database!!'));
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', function () {
+    console.log('Connected to MongoDB');
+});
+
+// mongoose.connect(config.mongoDb.host);
+// const db = mongoose.connection;
+// db.on('error', err => console.log("Error",err))
+// db.once('open',()=>console.info('Connected to the database!!'));
 
 
 const makeDetailsDb = require('./detailsDb')
