fbd94563ac3da8f021da5bbabd7420c512d7b236
MugishBeldar <mugishbeldar333@gmail.com>
2023-11-06 15:14:08 +0530

    added .env file

--- Detail Management Backend/data-access/index.js
+++ Detail Management Backend/data-access/index.js
@@ -1,7 +1,13 @@
+require('dotenv').config();
 const mysql2 = require("mysql2")
 const config = require("../config")
 const mongoose = require('mongoose');
 const DetailsModel = require('../migration-mongodb/detailsSchema');
+
+mongoose.connect(config.mongoDb.host);
+const db = mongoose.connection;
+db.on('error', err => console.log("Error",err))
+db.once('open',()=>console.info('Connected to the database!!'));
 
 const mysql = mysql2.createPool({
     host: config.mysql.host,
@@ -9,24 +15,6 @@ const mysql = mysql2.createPool({
     password: config.mysql.password,
     database: config.mysql.database
 }).promise();
-
-mongoose.connect(config.mongoDb.host, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', function () {
-    console.log('Connected to MongoDB');
-});
-
-// mongoose.connect(config.mongoDb.host);
-// const db = mongoose.connection;
-// db.on('error', err => console.log("Error",err))
-// db.once('open',()=>console.info('Connected to the database!!'));
-
 
 const makeDetailsDb = require('./detailsDb')
 const detailsDb = makeDetailsDb({mysql})
